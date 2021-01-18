c49d5f36f6527597393ecb68a54717b586253081
Evyatar Rosner <evyatar.rosner@tikalk.com>
2021-01-18 16:36:21 +0200

    update deps

--- src/server.js
+++ src/server.js
@@ -1,37 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const cors = require('cors');
 const port = 3000;
 
-app.use(cors());
-
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// parse application/json
 app.use(bodyParser.json());
 
-
-app.put('/user', (req, res) => {
-
+app.get('/', (req, res) => {
+	res.sendStatus(200);
 });
-
-app.get('/user', (req, res) => {
-});
-
-app.get('/user/:id', (req, res) => {
-
-});
-
-app.post('/user/:id', (req, res) => {
-
-});
-
-app.delete('/user/:id', (req, res) => {
-
-});
-
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
 
